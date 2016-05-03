70334e1d911502b12cd8dbf8a88cf03017f84f39
beermonster <leiweihuaba0@163.com>
2016-05-03 19:58:48 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+__author__ = 'lei'
+
+
+def addAllBit(num):
+	"""
+	add all bit num until sum less than 9
+	:param num: int number
+	:return:int number
+	"""
+	tempNum=num
+	s=11
+	while s>10:
+		s=0
+		while tempNum>0:
+			curbit=tempNum%10
+			s+=curbit
+			tempNum/=10
+
+		tempNum=s
+
+	return s
+
+def numToString(num):
+	"""
+
+	:param num:
+	:return:
+	"""
+
+if __name__=="__main__":
+	allsum=addAllBit(999)
+	print(allsum)
